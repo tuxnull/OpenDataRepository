e48923b246d7f12d7aa99c37822635883b7b4f45
tuxnull <amazonshitcarshow@setstorm.com>
2020-03-18 12:08:28 +0100

    Fixed typo in get_file.php

--- get_file.php
+++ get_file.php
@@ -2,7 +2,7 @@
 include("config.php");
 
 if(isset($_GET["id"])){
-	$array = mysqli_fetch_array(mysqli_query($mylink, "SELECT * FROM whatsapp_invites WHERE id = '".$_GET["id"]."'"));
+	$array = mysqli_fetch_array(mysqli_query($mylink, "SELECT * FROM data WHERE id = '".$_GET["id"]."'"));
 	echo '<iframe src="data:application/pdf;base64,'.$array["file"].'" height="100%" frameBorder="0" width="100%"></iframe>';
 }
 
