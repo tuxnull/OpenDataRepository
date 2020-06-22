b71391ee03df2b836ce0251c401209896adc8907
Patrick Garske <46957844+tuxnull@users.noreply.github.com>
2020-06-22 15:45:05 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?PHP
 session_start();
-define("MYSQL_HOST", "");
-define("MYSQL_DATABASE", "");
-define("MYSQL_USER", "");
-define("MYSQL_PASSWORD", "");
+define("MYSQL_HOST", "192.168.0.12");
+define("MYSQL_DATABASE", "alphagi_cloud");
+define("MYSQL_USER", "alphagi");
+define("MYSQL_PASSWORD", "Q2583EF");
 
 define("HOME_TITLE", "OpenDataRepository");
 
@@ -16,7 +16,7 @@ if(mysqli_connect_error()!=""){
 
 $qr = mysqli_query($mylink, "SHOW TABLES");
 if(!mysqli_num_rows($qr)>0){
-	mysqli_query($mylink,'CREATE TABLE data(id int NOT NULL AUTO_INCREMENT,name tinytext,link text,description text,file mediumtext, tagged int, PRIMARY KEY (id));');
+	mysqli_query($mylink,'CREATE TABLE data(id int NOT NULL AUTO_INCREMENT,name tinytext,link text,description text,file mediumtext,file_name mediumtext, tagged int, PRIMARY KEY (id));');
 	mysqli_query($mylink,'CREATE TABLE tags(id int NOT NULL AUTO_INCREMENT,name tinytext, PRIMARY KEY (id));');
 	mysqli_query($mylink,'CREATE TABLE authorization(id int NOT NULL AUTO_INCREMENT,username tinytext,hashed_password text,email text,permission_level int, PRIMARY KEY (id));');
 }
