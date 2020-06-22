cae5813be278fc4efbc229f6b6eb7ca6553c1dee
Patrick Garske <46957844+tuxnull@users.noreply.github.com>
2020-06-22 22:22:57 +0200

    Update get_file.php

--- get_file.php
+++ get_file.php
@@ -3,11 +3,19 @@ include("config.php");
 
 if(isset($_GET["id"])){
 	$array = mysqli_fetch_array(mysqli_query($mylink, "SELECT * FROM data WHERE id = '".$_GET["id"]."'"));
+	
+	
+	
 	if(!isset($_GET["download"])){
-		if(mb_strlen($array["file"])<100000){
+		
+		$f = finfo_open();
+
+		$mime_type = finfo_buffer($f, base64_decode($array["file"]), FILEINFO_MIME_TYPE);
+		
+		if($mime_type == "application/pdf"){
 			echo '<iframe src="data:application/pdf;base64,'.$array["file"].'" height="100%" frameBorder="0" width="100%"></iframe>';
 		}else{
-			echo '<h1>File is too large to be displayed.</h1><br>';	
+			echo '<h1>File can not be displayed.</h1><a href="./get_file.php?id='.$_GET["id"].'&download" download="'.$array["file_name"].'">Download File</a><br>';	
 		}
 	}else{
 		$f = finfo_open();
