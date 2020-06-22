518ed0ae3fa801aa9266ccd53862bcdfbaafec25
Patrick Garske <46957844+tuxnull@users.noreply.github.com>
2020-06-22 14:51:16 +0200

    Update get_file.php

--- get_file.php
+++ get_file.php
@@ -3,7 +3,11 @@ include("config.php");
 
 if(isset($_GET["id"])){
 	$array = mysqli_fetch_array(mysqli_query($mylink, "SELECT * FROM data WHERE id = '".$_GET["id"]."'"));
-	echo '<iframe src="data:application/pdf;base64,'.$array["file"].'" height="100%" frameBorder="0" width="100%"></iframe>';
+	if(mb_strlen($string, '8bit')<200000){
+		echo '<iframe src="data:application/pdf;base64,'.$array["file"].'" height="100%" frameBorder="0" width="100%"></iframe>';
+	}else{
+		echo '<h1>File is too large to be displayed.</h1>';	
+	}
 }
 
 
